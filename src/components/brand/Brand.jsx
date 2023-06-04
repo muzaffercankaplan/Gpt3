e69e0268abfc045f76ec8f325299b4115de96b36
muzaffer <muzafferckaplan@gmail.com>
2023-06-04 16:15:53 +0300

    added brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  const brandItem = [
+    {
+      src: google,
+      alt: "google",
+    },
+    {
+      src: atlassian,
+      alt: "atlassian",
+    },
+    {
+      src: dropbox,
+      alt: "dropbox",
+    },
+    {
+      src: shopify,
+      alt: "shopify",
+    },
+    {
+      src: slack,
+      alt: "slack",
+    },
+  ];
+  return (
+    <div className="brand sectionPadding">
+      {brandItem.map((item, index) => (
+        <div>
+          <img key={index} src={item.src} alt={item.alt} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Brand;
